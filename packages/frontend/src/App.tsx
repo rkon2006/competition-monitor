779bdcf19d85f480f4f3d5ea89dddf33ba9ccb63
Roman Kononikhin <rkon2006@gmail.com>
2026-05-01 17:31:39 +0200

    feat: basic react+vite app and routes

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import { AppListPage } from './components/AppListPage';
+import { AppDetailsPage } from './components/AppDetailsPage';
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppListPage />} />
+        <Route path="/apps/:id" element={<AppDetailsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ packages/frontend/src/components/AppDetailsPage.tsx
@@ -0,0 +1,3 @@
+export const AppDetailsPage = () => {
+  return <div>AppDetailsPage</div>;
+};
